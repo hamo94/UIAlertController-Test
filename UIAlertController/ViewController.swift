e213617a065790c9ed05c9f67dc14875a5a5da8f
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-16 15:36:15 +0900

    1st commit

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -15,11 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPressed(_ sender: Any) {
+        let myAlert = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다!!", preferredStyle: .alert)
+        
+        // 세번째 인자를 클로져 사용
+        let okAction = UIAlertAction(title: "종료", style: .default, handler: { (action: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.red
+        })
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: { (action: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.green
+        })
+        
+        myAlert.addAction(okAction)
+        myAlert.addAction(cancelAction)
+        
+        present(myAlert, animated: true, completion: nil)
     }
-
-
+    
 }
 
